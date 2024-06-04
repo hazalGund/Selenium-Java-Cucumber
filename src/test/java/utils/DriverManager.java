5954f58db02f43dbc7ab6f82fe92382783f609b6
Hazal Gunduz <hgunduz@mediafly.com>
2024-06-04 22:10:00 +0100

    add argument to chromeOptions

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -15,6 +15,7 @@ public class DriverManager {
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
                 driver = new ChromeDriver(options);
             } catch (Exception e) {
                 throw new RuntimeException("Failed to initialize WebDriver: " + e.getMessage());
